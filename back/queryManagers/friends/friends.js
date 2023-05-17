0b64d32b7423c19f197822b949aad21d4d1c0b4b
Ayoub <youbima17@gmail.com>
2023-05-17 19:35:48 +0200

    back friends

--- back/queryManagers/friends/friends.js
+++ back/queryManagers/friends/friends.js
@@ -1,17 +1,6 @@
 const mongoDBConnection = require("../mongoDBConnection");
 
 /**
- *
- * @fileoverview This file the manager of the friends API
- *
- * @author      Weel Ben Aissa
- * @author      Ayoub Imami
- * @author      Mourad Karrakchou
- *
- */
-
-
-/**
  * This function manages the request to the friends API
  * @param request url of the request
  * @param response
@@ -30,42 +19,49 @@ function manageRequest(request, response) {
         if (filePath[3] === "friendRequest") {
             request.on('end', function () {
                 const values = JSON.parse(body);
-                mongoDBConnection.friendRequest(response, values.from, values.friend);
+                mongoDBConnection.friendRequest(response, values.from, values.friend)
+                    .then(() => {console.log("Request: friendRequest")});
             });
         }
 
         else if (filePath[3] === "retrieveFriendList") {
             request.on('end', function () {
                 const values = JSON.parse(body);
-                mongoDBConnection.retrieveFriendList(response, values.token);
+                mongoDBConnection.retrieveFriendList(response, values.token)
+                    .then(() => {console.log("Request: retrieveFriendList")});
             });
         }
 
         else if (filePath[3] === "removeFriend") {
             request.on('end', function () {
                 const values = JSON.parse(body);
-                mongoDBConnection.removeFriend(response, values.token, values.friendToRemove);
+                mongoDBConnection.removeFriend(response, values.token, values.friendToRemove)
+                    .then(() => {console.log("Request: removeFriend")});
             });
         }
 
         else if (filePath[3] === "retrieveFriendRequest") {
             request.on('end', function () {
                 const values = JSON.parse(body);
-                mongoDBConnection.retrieveFriendRequest(response, values.token);
+                mongoDBConnection.retrieveFriendRequest(response, values.token)
+                    .then(() => {console.log("Request: retrieveFriendRequest")});
             });
         }
 
         else if (filePath[3] === "acceptFriendRequest") {
             request.on('end', function () {
                 const values = JSON.parse(body);
-                mongoDBConnection.acceptFriendRequest(response, values.token, values.friendToAccept);
+                mongoDBConnection.acceptFriendRequest(response, values.token, values.friendToAccept)
+                    .then(() => {console.log("Request: acceptFriendRequest")});
             });
         }
 
         else if (filePath[3] === "declineFriendRequest") {
             request.on('end', function () {
                 const values = JSON.parse(body);
-                mongoDBConnection.declineFriendRequest(response, values.token, values.friendToDecline);});
+                mongoDBConnection.declineFriendRequest(response, values.token, values.friendToDecline)
+                    .then(() => {console.log("Request: declineFriendRequest")});
+            });
         }
     }
     else{
